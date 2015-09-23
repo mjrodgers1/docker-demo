4ac3edaab4e1b4cb0041a7d9e5c0140e6af3d400
mjrodgers1 <mjrodgers1@gmail.com>
2015-09-23 17:45:04 +0100

    post first commit edits
    specified the correct web address
    changed the webservice message

--- service.js
+++ service.js
@@ -1,7 +1,6 @@
 var http = require('http');
-var server=http.createServer(function(request, response){ console.log('i think .');
+var server=http.createServer(function(request, response){ console.log('hello .');
 response.writeHead(
-200, {"Content-Type": "application/json"}); var data = { 'message' : 'therefor i am' }; response.end(JSON.stringify(data) + '\n');
+200, {"Content-Type": "application/json"}); var data = { 'message' : 'from source control!' }; response.end(JSON.stringify(data) + '\n');
 });
-
-server.listen(8888); console.log('node web service FIRST...');
+server.listen(8888); console.log('github based web service...');
